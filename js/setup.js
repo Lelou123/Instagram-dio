af54ca20a0e8e71c9ac42910adc9b07a9e5794dc
Lelou123 <murillosama1@hotmail.com>
2021-08-29 17:52:54 -0300

    Add Modo Claro

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,5 @@
 function Troca (){
-
-    var cor=document.getElementById("body");
-    $("body").css("background-color", "rgb(255, 255, 255)");
+    $("body").css("background", "rgb(173, 166, 166)");
+    
 }
+
